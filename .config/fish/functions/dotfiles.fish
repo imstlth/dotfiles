f141bfba12c7a98a1a063a5f3e650cf893a5a005
caracole <plesourd@proton.me>
2026-04-21 11:18:38 +0200

    test

--- .config/fish/functions/dotfiles.fish
+++ .config/fish/functions/dotfiles.fish
@@ -8,7 +8,7 @@ function dotfiles
     echo "Une fois que t'as copié ton mdp, appuis sur entrée pour passer à la suite"
     read -P ""
     echo "ton username est 'imstlth'"
-    git push --set-upstream main main
+    git push
     mv .git/ .git_/
     echo "Fini !"
 end
