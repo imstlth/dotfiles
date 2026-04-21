c84c1e63c60381f816b203c4fa9ce61314437384
caracole <plesourd@proton.me>
2026-04-21 11:21:36 +0200

    On push sur la bonne branche

--- .config/fish/functions/dotfiles.fish
+++ .config/fish/functions/dotfiles.fish
@@ -8,7 +8,7 @@ function dotfiles
     echo "Une fois que t'as copié ton mdp, appuis sur entrée pour passer à la suite"
     read -P ""
     echo "ton username est 'imstlth'"
-    git push
+    git push --set-upstream master main
     mv .git/ .git_/
     echo "Fini !"
 end
